aa93f152936ddb206f2653fe912864df52473c07
xventrux <vvyacheslavx@gmail.com>
2023-09-24 18:14:30 +0300

    feat: create book entity

---
+++ API.Domain/Entities/Base/EntityBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Domain.Entities.Base
+{
+    /// <summary>
+    /// Базовая сущность
+    /// </summary>
+    public class EntityBase
+    {
+        /// <summary>
+        /// Идентификатор сущности
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Дата и время создания сущности
+        /// </summary>
+        public DateTime CreationDate { get; set; }
+
+        /// <summary>
+        /// Дата и время обновления сущности
+        /// </summary>
+        public DateTime UpdateDate { get; set; }
+    }
+}
---
+++ API.Domain/Entities/Book.cs
@@ -0,0 +1,30 @@
+﻿using API.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Domain.Entities
+{
+    /// <summary>
+    /// Книга
+    /// </summary>
+    public class Book : EntityBase
+    {
+        /// <summary>
+        /// Заголовок книги
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Описание книги
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Автор книги
+        /// </summary>
+        public string Author { get; set; }
+    }
+}
