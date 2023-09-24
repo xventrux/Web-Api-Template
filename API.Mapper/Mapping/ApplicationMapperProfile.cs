2acf849f17c74ee30accb669cfc7f5002cc25feb
xventrux <vvyacheslavx@gmail.com>
2023-09-24 19:34:33 +0300

    feat: add mapper profile

---
+++ API.Mapper/Mapping/ApplicationMapperProfile.cs
@@ -0,0 +1,27 @@
+﻿using API.Contracts.DTOs.BookDTOs;
+using API.Domain.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Mapper.Mapping
+{
+    public class ApplicationMapperProfile : Profile
+    {
+        public ApplicationMapperProfile()
+        {
+            configureBookModels();
+        }
+
+        private void configureBookModels()
+        {
+            CreateMap<CreateBookRequest, Book>();
+            CreateMap<EditBookRequest, Book>();
+            CreateMap<Book, GetManyBooksResponse>();
+            CreateMap<Book, GetOneBookResponse>();
+        }
+    }
+}
