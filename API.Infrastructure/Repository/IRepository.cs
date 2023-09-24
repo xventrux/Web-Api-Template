737488563e354ac8f200034dbb51ab37137a9cff
xventrux <vvyacheslavx@gmail.com>
2023-09-24 18:46:07 +0300

    feat: implement repository

---
+++ API.Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.Repository
+{
+    /// <summary>
+    /// Репозиторий
+    /// </summary>
+    /// <typeparam name="TEntity">Сущность</typeparam>
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Возвращает все записи
+        /// </summary>
+        /// <returns></returns>
+        IQueryable<TEntity> GetAll();
+
+        /// <summary>
+        /// Возвращает отфильтрованные записи
+        /// </summary>
+        /// <param name="predicat"></param>
+        /// <returns></returns>
+        IQueryable<TEntity> GetAllFiltered(Expression<Func<TEntity, bool>> predicat);
+        /// <summary>
+        /// Возвращает запись по идентификатору
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<TEntity> GetByIdAsync(Guid id);
+
+        /// <summary>
+        /// Добавляет запись
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        Task AddAsync(TEntity model);
+
+        /// <summary>
+        /// Обновляет информацию записи
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        Task UpdateAsync(TEntity model);
+
+        /// <summary>
+        /// Удалаяет запись
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task DeleteAsync(TEntity model);
+
+        /// <summary>
+        /// Сохраняет изменения
+        /// </summary>
+        /// <returns></returns>
+        Task SaveChangesAsync();
+    }
+}
---
+++ API.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.Repository
+{
+    /// <inheritdoc/>
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected DbContext DbContext { get; }
+        protected DbSet<TEntity> DbSet { get; }
+
+        public Repository(DbContext dbContext)
+        {
+            DbContext = dbContext;
+            DbSet = DbContext.Set<TEntity>();
+        }
+
+        /// <inheritdoc/>
+        public IQueryable<TEntity> GetAll()
+        {
+            return DbSet;
+        }
+        /// <inheritdoc/>
+        public IQueryable<TEntity> GetAllFiltered(Expression<Func<TEntity, bool>> predicat)
+        {
+            if (predicat == null) throw new ArgumentNullException(nameof(predicat));
+            return DbSet.Where(predicat);
+        }
+        /// <inheritdoc/>
+        public async Task<TEntity> GetByIdAsync(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<TEntity> GetByIdAsync(long id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        /// <inheritdoc/>
+        public Task AddAsync(TEntity model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+            DbSet.Add(model);
+            return SaveChangesAsync();
+        }
+        /// <inheritdoc/>
+        public Task UpdateAsync(TEntity model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+            DbSet.Update(model);
+            return SaveChangesAsync();
+        }
+        /// <inheritdoc/>
+        public Task DeleteAsync(TEntity model)
+        {
+            if (model == null) throw new ArgumentNullException(nameof(model));
+            DbSet.Remove(model);
+            return SaveChangesAsync();
+        }
+        /// <inheritdoc/>
+        public Task SaveChangesAsync()
+        {
+            return DbContext.SaveChangesAsync();
+        }
+    }
+}
