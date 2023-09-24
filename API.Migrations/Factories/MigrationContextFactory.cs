bc9f2d2d27b421bda62e750310646e4b9df4c2bc
xventrux <vvyacheslavx@gmail.com>
2023-09-24 18:26:28 +0300

    feat: create migration factory

---
+++ API.Migrations/Factories/MigrationContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Migrations.Factories
+{
+    public class MigrationContextFactory : IDesignTimeDbContextFactory<MigrationDbContext>
+    {
+        public MigrationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MigrationDbContext>();
+
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+
+            IConfigurationRoot config = builder.Build();
+
+            string connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString, opts => opts.CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds));
+
+            return new MigrationDbContext(optionsBuilder.Options);
+
+        }
+    }
+}
---
+++ API.Migrations/MigrationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using API.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Migrations
+{
+    public class MigrationDbContext : BaseDbContext
+    {
+        public MigrationDbContext(DbContextOptions options) : base(options) { }
+    }
+}
