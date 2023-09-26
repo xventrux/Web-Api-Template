461f7bf1078bb5adde5881d5ed1b0fc4e25ce6a3
xventrux <vvyacheslavx@gmail.com>
2023-09-26 07:11:11 +0300

    feat: registrate book services

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
+using API;
 using API.DataAccess;
 using API.Infrastructure.Repository;
 using API.Mapper.Mapping;
+using API.AppServices.Services.BookServices;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,8 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<DbContext>(s => s.GetRequiredService<BaseDbContext>());
 builder.Services.AddAutoMapper(typeof(ApplicationMapperProfile));
+
 //Регистрация репозитория
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddTransient<IBookService, BookService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
